e293f90d5376ec3baf2358956c3880b7f31e26f1
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-02-04 20:32:28 +0100

    storybook

--- packages/flowbite/.storybook/preview.js
+++ packages/flowbite/.storybook/preview.js
@@ -1,4 +1,4 @@
-import "../src/styles/globals.css";
+import "./style.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
---
+++ packages/flowbite/.storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+  brandTitle: "React Last-Field - atomic form library",
+  brandImage: "./last-field.png",
+});
