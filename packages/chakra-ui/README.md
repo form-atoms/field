89f5686eb129494a707af092e0d766802a9c725e
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-02-28 19:16:47 +0100

    chore: drop chakra
    extract to https://github.com/MiroslavPetrik/form-atoms-chakra-ui

--- packages/chakra-ui/README.md
+++
@@ -1,4 +0,0 @@
-<div align="center">
-  <img width="120" style="margin: 32px" src="../../form-atoms-field.svg">
-  <h1>@form-atoms/chakra-ui</h1>
-</div>
--- packages/chakra-ui/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./checkbox-field";
-export * from "./file-field";
-export * from "./number-field";
-export * from "./radio-field";
-export * from "./range-field";
-export * from "./text-field";
