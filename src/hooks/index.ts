a2f84bd9831b89a1fe8c35900f779afea57979c4
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-03-05 13:18:43 +0100

    fix: export useOptionFieldProps

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,7 @@ export * from "./use-clear-file-input-effect";
 export * from "./use-clear-input-action";
 export * from "./use-field-error";
 export * from "./use-field-props";
+export * from "./use-option-field-props";
 export * from "./use-options";
 export * from "./use-required-props";
 export * from "./use-select-options";
