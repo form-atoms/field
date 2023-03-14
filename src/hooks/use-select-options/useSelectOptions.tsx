004dd8996203108aee35256a5f9de9b1fb86402f
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-03-14 16:03:23 +0100

    fix: react warning about select option being selected
    not needed, as value is for real set on the <select /> now with -1 being the empty value.

--- src/hooks/use-select-options/useSelectOptions.tsx
+++ src/hooks/use-select-options/useSelectOptions.tsx
@@ -22,7 +22,7 @@ export function useSelectOptions<Option>({
       selectOptions: (
         <>
           {placeholder && (
-            <option value={EMPTY_VALUE} disabled selected>
+            <option value={EMPTY_VALUE} disabled>
               {placeholder}
             </option>
           )}
