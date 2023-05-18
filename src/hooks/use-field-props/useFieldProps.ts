334d276bf08b36e6a012caa60164cff68b161712
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-05-18 14:24:26 +0200

    fix(#34): for required field, indicate aria-valid only after validation

--- src/hooks/use-field-props/useFieldProps.ts
+++ src/hooks/use-field-props/useFieldProps.ts
@@ -30,9 +30,14 @@ export function useFieldProps<
   const field = useAtomValue(fieldAtom);
   const name = useAtomValue(field.name);
   const required = useAtomValue(field.required);
+  const validationCount = useAtomValue(field._validateCount);
   const validate = useSetAtom(field.validate);
   const ref = useSetAtom(field.ref);
   const [, startTransition] = useTransition();
+
+  const ariaInvalid = state.validateStatus === "invalid";
+  // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-invalid
+  const requiredAriaInvalid = validationCount > 0 ? ariaInvalid : undefined;
 
   return useMemo(
     () => ({
@@ -41,7 +46,7 @@ export function useFieldProps<
       value: state.value ?? empty,
       required,
       "aria-required": required,
-      "aria-invalid": state.validateStatus === "invalid",
+      "aria-invalid": required ? requiredAriaInvalid : ariaInvalid,
       ref,
       onBlur() {
         actions.setTouched(true);
@@ -60,6 +65,15 @@ export function useFieldProps<
         });
       },
     }),
-    [state, actions, name, required, getEventValue, ref, validate]
+    [
+      state,
+      actions,
+      name,
+      required,
+      validationCount,
+      getEventValue,
+      ref,
+      validate,
+    ]
   );
 }
