5579e7b9a6e4507edf9a8ac518970e8dd4007f9e
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-06-01 15:57:07 +0200

    test: stringArrayField spec

---
+++ src/fields/array-field/stringArrayField.test-d.ts
@@ -0,0 +1,25 @@
+import { formAtom } from "form-atoms";
+import { expectTypeOf } from "vitest";
+
+import { stringArrayField } from "./stringArrayField";
+import { FormSubmitValues } from "../zodField";
+
+test("required stringArrayField has '[string, ...string[]]' submit value", () => {
+  const form = formAtom({
+    field: stringArrayField(),
+  });
+
+  expectTypeOf<FormSubmitValues<typeof form>>().toEqualTypeOf<{
+    field: [string, ...string[]];
+  }>();
+});
+
+test("optional stringArrayField has 'string[]' submit value", () => {
+  const form = formAtom({
+    field: stringArrayField().optional(),
+  });
+
+  expectTypeOf<FormSubmitValues<typeof form>>().toEqualTypeOf<{
+    field: string[];
+  }>();
+});
