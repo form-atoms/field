efda2b606abbafc541e20a3e71e45449fa46618a
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-06-02 08:05:38 +0200

    test: numberField spec

---
+++ src/fields/number-field/numberField.test-d.ts
@@ -0,0 +1,25 @@
+import { formAtom } from "form-atoms";
+import { expectTypeOf } from "vitest";
+
+import { numberField } from "./numberField";
+import { FormSubmitValues } from "../zodField";
+
+test("required numberField has 'number' submit value", () => {
+  const form = formAtom({
+    field: numberField(),
+  });
+
+  expectTypeOf<FormSubmitValues<typeof form>>().toEqualTypeOf<{
+    field: number;
+  }>();
+});
+
+test("optional numberField has 'number | undefined' submit value", () => {
+  const form = formAtom({
+    field: numberField().optional(),
+  });
+
+  expectTypeOf<FormSubmitValues<typeof form>>().toEqualTypeOf<{
+    field: number | undefined;
+  }>();
+});
