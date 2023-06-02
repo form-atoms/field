a470bdd6bd3b3bf66671265cc2c7a63fb09126de
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-06-02 16:47:34 +0200

    test(ListField): remove & add actions

--- src/components/list-field/ListField.test.tsx
+++ src/components/list-field/ListField.test.tsx
@@ -1,9 +1,11 @@
 import { act, render, renderHook, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { InputField, formAtom, useFormSubmit } from "form-atoms";
 import { describe, expect, it, vi } from "vitest";
 
 import { ListField } from "./ListField";
 import { numberField, textField } from "../../fields";
+import { NumberInput } from "../../fields/number-field/NumberInput";
 
 describe("<ListField />", () => {
   it("works with flat list of fields", async () => {
@@ -55,6 +57,35 @@ describe("<ListField />", () => {
       expect(removeItemButton).toBeInTheDocument();
       expect(removeItemButton).toHaveAttribute("type", "button");
     });
+
+    it("removes the respective list item", async () => {
+      const form = formAtom({
+        luckyNumbers: [numberField({ value: 3 })],
+      });
+
+      render(
+        <ListField
+          path={["luckyNumbers"]}
+          form={form}
+          builder={() => numberField()}
+        >
+          {({ fields, RemoveItemButton }) => (
+            <>
+              <NumberInput field={fields} label="" /> <RemoveItemButton />
+            </>
+          )}
+        </ListField>
+      );
+
+      const removeItemButton = screen.getByText("Remove");
+
+      expect(removeItemButton).toBeInTheDocument();
+      expect(screen.queryByDisplayValue("3")).toBeInTheDocument();
+
+      await act(() => userEvent.click(removeItemButton));
+
+      expect(screen.queryByDisplayValue("3")).not.toBeInTheDocument();
+    });
   });
 
   describe("AddItemButton", () => {
@@ -77,6 +108,33 @@ describe("<ListField />", () => {
 
       expect(addItemButton).toBeInTheDocument();
       expect(addItemButton).toHaveAttribute("type", "button");
+    });
+
+    it("appends empty item to the list by calling the item builder prop", async () => {
+      const form = formAtom({
+        luckyNumbers: [numberField({ value: 3 })],
+      });
+
+      const builder = vi.fn(() => numberField({ value: 6 }));
+
+      render(
+        <ListField path={["luckyNumbers"]} form={form} builder={builder}>
+          {({ fields }) => (
+            <>
+              <NumberInput field={fields} label="lucky" />
+            </>
+          )}
+        </ListField>
+      );
+
+      const addItemButton = screen.getByText("Add item");
+
+      expect(addItemButton).toBeInTheDocument();
+
+      await act(() => userEvent.click(addItemButton));
+
+      expect(screen.queryByDisplayValue("6")).toBeInTheDocument();
+      expect(screen.queryAllByLabelText("lucky")).toHaveLength(2);
     });
   });
 });
