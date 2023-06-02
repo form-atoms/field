9741bf916f73227f9c6cea33056d8ca9e17cd12e
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-06-02 14:32:25 +0200

    test(ListField): add & remove button default props

--- src/components/list-field/ListField.test.tsx
+++ src/components/list-field/ListField.test.tsx
@@ -3,7 +3,7 @@ import { InputField, formAtom, useFormSubmit } from "form-atoms";
 import { describe, expect, it, vi } from "vitest";
 
 import { ListField } from "./ListField";
-import { textField } from "../../fields";
+import { numberField, textField } from "../../fields";
 
 describe("<ListField />", () => {
   it("works with flat list of fields", async () => {
@@ -28,5 +28,55 @@ describe("<ListField />", () => {
     });
 
     expect(onSubmit).toHaveBeenCalledWith({ friends: ["Bryan", "Alice"] });
+  });
+
+  describe("RemoveItemButton", () => {
+    it("has 'Remove' label by default", () => {
+      const form = formAtom({
+        luckyNumbers: [numberField({ value: 3 })],
+      });
+
+      render(
+        <ListField
+          path={["luckyNumbers"]}
+          form={form}
+          builder={() => numberField()}
+        >
+          {({ RemoveItemButton }) => (
+            <>
+              <RemoveItemButton />
+            </>
+          )}
+        </ListField>
+      );
+
+      const removeItemButton = screen.getByText("Remove");
+
+      expect(removeItemButton).toBeInTheDocument();
+      expect(removeItemButton).toHaveAttribute("type", "button");
+    });
+  });
+
+  describe("AddItemButton", () => {
+    it("has 'Add item' label by default", () => {
+      const form = formAtom({
+        luckyNumbers: [numberField({ value: 3 })],
+      });
+
+      render(
+        <ListField
+          path={["luckyNumbers"]}
+          form={form}
+          builder={() => numberField()}
+        >
+          {() => <></>}
+        </ListField>
+      );
+
+      const addItemButton = screen.getByText("Add item");
+
+      expect(addItemButton).toBeInTheDocument();
+      expect(addItemButton).toHaveAttribute("type", "button");
+    });
   });
 });
