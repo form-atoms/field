0612bab3abf61ea4fb64d9ef861642139b2b4fd9
Miroslav Petrik <miroslavpetrik@outlook.com>
2023-06-12 14:45:49 +0200

    test: useOptions test for id scoping

---
+++ src/hooks/use-options/useOptions.test.ts
@@ -0,0 +1,26 @@
+import { renderHook } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+import { useOptions } from "./useOptions";
+import { booleanField } from "../../fields";
+
+describe("useOptions()", () => {
+  it("scopes the option ids by the fieldId", () => {
+    const field = booleanField();
+
+    const { result } = renderHook(() =>
+      useOptions({
+        field,
+        options: [false, true],
+        getLabel: (yes) => (yes ? "yes" : "no"),
+      })
+    );
+
+    const fieldId = `${field}`;
+
+    expect(result.current.renderOptions).toEqual([
+      { id: `${fieldId}/0`, value: 0, label: "no" },
+      { id: `${fieldId}/1`, value: 1, label: "yes" },
+    ]);
+  });
+});
