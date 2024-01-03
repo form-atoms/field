9291fe31812fb500ae305e716d57dd4df1328d78
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-01-03 18:26:13 +0100

    docs(ListField): extract radio control

--- src/components/list-field/ListField.stories.tsx
+++ src/components/list-field/ListField.stories.tsx
@@ -1,10 +1,9 @@
 import { InputField, fieldAtom } from "form-atoms";
 
 import { AddButtonProps, ListField, RemoveButtonProps } from "./ListField";
-import { checkboxField, listField, textField } from "../../fields";
+import { listField, textField } from "../../fields";
 import { formStory, meta } from "../../scenarios/StoryForm";
 import { FieldLabel } from "../field-label";
-import { Radio, RadioControl } from "../radio";
 
 export default {
   ...meta,
@@ -250,7 +249,7 @@ export const Ordering = formStory({
   },
 });
 
-export const Nested = formStory({
+export const NestedListField = formStory({
   args: {
     fields: {
       users: listField({
@@ -359,79 +358,6 @@ export const Nested = formStory({
           </article>
         )}
       </ListField>
-    ),
-  },
-});
-
-export const WithRadioControl = formStory({
-  args: {
-    fields: {
-      phones: listField({
-        value: [
-          {
-            number: "+421 200 300 500",
-            isPrimary: true,
-          },
-        ],
-        builder: ({ number }) => ({
-          number: fieldAtom({ value: number }),
-          isPrimary: checkboxField({ name: "primaryPhone" }).optional(),
-        }),
-      }),
-    },
-    children: ({ fields }) => (
-      <RadioControl>
-        {({ control }) => (
-          <ListField
-            field={fields.phones}
-            AddButton={({ add }) => (
-              <button type="button" className="outline" onClick={add}>
-                Add contact phone
-              </button>
-            )}
-            RemoveButton={RemoveButton}
-          >
-            {({ fields, RemoveButton }) => (
-              <>
-                <div
-                  style={{
-                    display: "grid",
-                    gridGap: 16,
-                    gridTemplateColumns: "auto min-content",
-                  }}
-                >
-                  <InputField
-                    atom={fields.number}
-                    render={(props) => (
-                      <input
-                        {...props}
-                        placeholder="Phone number in format +421 xxx xxx xxx"
-                      />
-                    )}
-                  />
-
-                  <RemoveButton />
-                </div>
-                <Radio control={control} field={fields.isPrimary}>
-                  {(props) => (
-                    <div style={{ marginBottom: 40 }}>
-                      <input
-                        {...props}
-                        type="radio"
-                        id={`${fields.isPrimary}`}
-                      />
-                      <FieldLabel
-                        label="Primary, receive 2FA SMS on this phone."
-                        field={fields.isPrimary}
-                      />
-                    </div>
-                  )}
-                </Radio>
-              </>
-            )}
-          </ListField>
-        )}
-      </RadioControl>
     ),
   },
 });
