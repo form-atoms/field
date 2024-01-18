256cc1475630c41bc80c867d8cf06eb6cae58771
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-01-18 14:47:43 +0100

    fix(useTextFieldProps): make textField initializable via options (#88)

---
+++ src/fields/text-field/textField.stories.tsx
@@ -0,0 +1,45 @@
+import { textField } from "./textField";
+import { TextInput } from "./TextInput.mock";
+import { formStory, meta } from "../../scenarios/StoryForm";
+
+export default {
+  ...meta,
+  title: "fields/textField",
+};
+
+export const Required = formStory({
+  args: {
+    fields: {
+      name: textField({ name: "name" }),
+    },
+    children: ({ fields }) => (
+      <TextInput field={fields.name} label="Your Name" />
+    ),
+  },
+});
+
+export const Optional = formStory({
+  args: {
+    fields: {
+      nick: textField({ name: "nick" }).optional(),
+    },
+    children: ({ fields }) => (
+      <TextInput field={fields.nick} label="Your Nick" />
+    ),
+  },
+});
+
+export const Initialized = formStory({
+  args: {
+    fields: {
+      title: textField({ name: "title" }).optional(),
+    },
+    children: ({ fields }) => (
+      <TextInput
+        field={fields.title}
+        label="Blog Post Title"
+        initialValue="How to initialize input"
+      />
+    ),
+  },
+});
