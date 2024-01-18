83986190520937ab54aa9ade780dc445805e4964
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-01-18 14:37:27 +0100

    fix(useFilesFieldProps): ignore the initialValue (#86)

--- src/hooks/use-files-field-props/useFilesFieldProps.ts
+++ src/hooks/use-files-field-props/useFilesFieldProps.ts
@@ -1,16 +1,22 @@
+import { UseFieldOptions } from "form-atoms";
 import { ChangeEvent } from "react";
 
 import { FieldProps, useFieldProps } from "../";
-import { type FilesField } from "../../fields";
+import type { FilesField, FilesFieldValue } from "../../fields";
 
-export type FileFieldProps = FieldProps<FilesField>;
+// Usually we don't have File objects ready, when an item is being edited.
+// So the initialValue is not needed for the filesField
+export type FilesFieldProps = Omit<FieldProps<FilesField>, "initialValue">;
 
 const getFiles = (event: ChangeEvent<HTMLInputElement>) =>
   Array.from(event.currentTarget.files ?? []);
 
-export const useFilesFieldProps = (field: FilesField) => {
+export const useFilesFieldProps = (
+  field: FilesField,
+  options?: Omit<UseFieldOptions<FilesFieldValue>, "initialValue">,
+) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { value, ...props } = useFieldProps(field, getFiles);
+  const { value, ...props } = useFieldProps(field, getFiles, options);
 
   return props;
 };
