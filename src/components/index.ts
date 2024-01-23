34bb722c2e81b17857149cb46328d76159bd3f67
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-01-23 14:51:28 +0100

    fix(file-upload): add export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./checkbox-group";
 export * from "./field-errors";
 export * from "./field-label";
+export * from "./file-upload";
 export * from "./list";
 export * from "./multi-select";
 export * from "./placeholder-option";
