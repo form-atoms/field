6f82e0ad7cd7602874cc260e961524aee696b961
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-02-14 13:18:11 +0100

    fix type

--- src/hooks/use-list-field/useListFieldInitialValue.ts
+++ src/hooks/use-list-field/useListFieldInitialValue.ts
@@ -1,4 +1,4 @@
-import { FieldAtom, RESET, UseFieldInitialValueOptions } from "form-atoms";
+import { FieldAtom, RESET, UseAtomOptions } from "form-atoms";
 
 import { _useFieldInitialValue } from "../../atoms";
 import { useHydrateField } from "../use-hydrate-field";
@@ -6,7 +6,7 @@ import { useHydrateField } from "../use-hydrate-field";
 export function useListFieldInitialValue<Value>(
   fieldAtom: FieldAtom<Value>,
   initialValue?: Value | typeof RESET,
-  options?: UseFieldInitialValueOptions<Value>,
+  options?: UseAtomOptions,
 ): void {
   useHydrateField(fieldAtom, initialValue);
   _useFieldInitialValue(fieldAtom, initialValue, options);
