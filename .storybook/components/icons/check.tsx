14a7f06b481bf0564ae6c878992748a2940b7122
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-02-26 16:19:43 +0100

    docs: code element with copy to clipboard

---
+++ .storybook/components/icons/check.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function Check({
+  isAnimated = false,
+  strokeWidth = 4,
+  ...props
+}) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth={strokeWidth}
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon-check"
+      {...props}
+    >
+      {isAnimated && (
+        <style>{`
+            path#check-icon {
+              stroke-dasharray: 22px;
+              stroke-dashoffset: 22px;
+              animation: check 0.2s ease-out forwards;
+            }
+  
+            @keyframes check {
+              from {
+                stroke-dashoffset: 22px;
+              }
+              to {
+                stroke-dashoffset: 0;
+              }
+            }
+          `}</style>
+      )}
+      <path id="check-icon" d="M5 12l5 5l10 -10"></path>
+    </svg>
+  );
+}
---
+++ .storybook/components/icons/copy.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Copy(props: any) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      strokeWidth="2"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="icon-copy"
+      {...props}
+    >
+      <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+      <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+    </svg>
+  );
+}
