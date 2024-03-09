587ec5b59a928fbd0cc00cc47652048298aa312a
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-09 17:49:38 +0100

    docs: disable `show code` button (#119)
    It crashes the page, as some theme variables are not usable.
    It was not usable anyway, as it displayed processed storybook code, not practically usable.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -26,6 +26,9 @@ export default {
       },
     },
     docs: {
+      canvas: {
+        sourceState: "none",
+      },
       theme: darkTheme,
       container: PicoContainer,
       components: {
