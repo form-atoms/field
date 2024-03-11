25d95b9ee43148eb39a1ff762af66b0d7a6ffd98
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-11 19:22:34 +0100

    configure arrayField

--- src/fields/array-field/arrayField.ts
+++ src/fields/array-field/arrayField.ts
@@ -1,35 +1,39 @@
-import { ArrayCardinality, ZodAny, ZodArray, ZodSchema, z } from "zod";
+import type { ArrayCardinality, ZodAny, ZodArray, ZodSchema } from "zod";
+import { z } from "zod";
 
-import {
-  ZodField,
-  ZodFieldConfig,
-  ZodParams,
-  defaultParams,
-  zodField,
-} from "../zod-field";
+import { prepareSchema } from "../../utils";
+import { FieldConfig } from "../field";
+import { type ZodField, defaultParams, zodField } from "../zod-field";
 
 export type ZodArrayField<Element extends ZodSchema = ZodAny> = ZodField<
   ZodArray<Element, ArrayCardinality>,
   ZodArray<Element, ArrayCardinality>
 >;
 
-export type ArrayFieldParams<ElementSchema extends z.Schema> = Partial<
-  ZodFieldConfig<
-    ZodArray<ElementSchema, "atleastone">,
-    ZodArray<ElementSchema, "many">
-  >
-> &
-  ZodParams;
+export type ArrayFieldParams<ElementSchema extends z.Schema> = FieldConfig<
+  ZodArray<ElementSchema, "atleastone">,
+  ZodArray<ElementSchema, "many">
+>;
 
 export const arrayField = <ElementSchema extends z.Schema>({
   required_error = defaultParams.required_error,
   value = [],
   elementSchema,
+  schema,
+  optionalSchema,
   ...config
 }: { elementSchema: ElementSchema } & ArrayFieldParams<ElementSchema>) =>
   zodField({
     value,
-    schema: z.array(elementSchema).nonempty(required_error),
-    optionalSchema: z.array(elementSchema),
+    ...prepareSchema({
+      initial: {
+        schema: z.array(elementSchema).nonempty(required_error),
+        optionalSchema: z.array(elementSchema),
+      },
+      user: {
+        schema,
+        optionalSchema,
+      },
+    }),
     ...config,
   });
