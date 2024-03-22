289479b235e69057ca84993327fe66d9ce08832d
Miroslav Petrik <miroslavpetrik@outlook.com>
2024-03-22 11:25:29 +0100

    test(useNumberFieldProps): cover with test

---
+++ src/hooks/use-number-field-props/useNumberFieldProps.test.tsx
@@ -0,0 +1,46 @@
+import { act, render, renderHook, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { useFieldValue } from "form-atoms";
+import { describe, expect, it } from "vitest";
+
+import { useNumberFieldProps } from "./useNumberFieldProps";
+import { numberField } from "../../fields";
+
+describe("useNumberFieldProps()", () => {
+  it("initializes the field via options", async () => {
+    const field = numberField();
+
+    const props = renderHook(() =>
+      useNumberFieldProps(field, { initialValue: 42 }),
+    );
+
+    expect(props.result.current.value).toBe(42);
+  });
+
+  it("reads empty input value as undefined with props.value as empty string", async () => {
+    const field = numberField({ value: 10 });
+
+    const { result: props } = renderHook(() => useNumberFieldProps(field));
+    const { result: value } = renderHook(() => useFieldValue(field));
+    render(<input {...props.current} />);
+
+    expect(props.current.value).toBe(10);
+    expect(value.current).toBe(10);
+
+    await act(() => userEvent.clear(screen.getByRole("textbox")));
+
+    expect(props.current.value).toBe("");
+    expect(value.current).toBe(undefined);
+  });
+
+  it("reads numeric value as Number", async () => {
+    const field = numberField();
+
+    const { result: props } = renderHook(() => useNumberFieldProps(field));
+    render(<input type="number" {...props.current} />);
+
+    await act(() => userEvent.type(screen.getByRole("spinbutton"), "9"));
+
+    expect(props.current.value).toBe(9);
+  });
+});
